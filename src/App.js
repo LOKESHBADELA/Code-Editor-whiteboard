bb7e0813e4b334ee8a204ccd25c5e3a387bd86ec
Lokesh Badela <lakkubaddu1117@gmail.com>
2024-10-07 19:35:47 +0530

    first-commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PlayGround from "./Components/PlayGround/PlayGround";
+import "../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css"; // eslint-disable-line no-unused-vars
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <PlayGround />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/PlayGround/Language/Default.js
@@ -0,0 +1,36 @@
+const allLanguageDefaultCode = {
+  cpp: `#include<iostream>
+using namespace std;
+int main()
+{
+  // Start writing your code from here!
+  cout<<"Welcome to Edicode!";
+  return 0;
+}
+`,
+  c: `#include <stdio.h>
+int main()
+{
+  // Start writing your code from here!
+	printf("Welcome to Edicode");
+	return 0;
+}   
+    `,
+  java: `import java.util.*;
+class Main
+{
+    public static void main(String[] args)
+    {
+        // Start writing your code from here!
+        System.out.print("Welcome to Edicode");
+    }
+}
+    `,
+  python: `# Start writing your code from here!
+print("Welcome to Edicode")
+    `,
+  javascript: `// Start writing your code from here!
+console.log("Welcome to Edicode")
+    `,
+};
+export default allLanguageDefaultCode;
